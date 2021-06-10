e3fa4ee0de55cb4b701efa395c4aa4a471e485c9
Ivan Apostolov <Ivan.Apostolov@playtech.com>
2021-06-10 15:30:26 +0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
    return (
       <footer className="ft__footer">
          <div className="container">
-            <span className="text-muted">Place sticky footer content here.</span>
+            <span className="text-muted">Copyright © 2021 "ТИП ТОП 21" Всички права запазени.</span>
          </div>
       </footer>
    );
